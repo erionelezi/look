9d9829c87dd155365548c84e7d148fefd8664a12
Erion Elezi <erion.elezi1@yahoo.com>
2020-04-07 22:39:02 +0200

    perfundime fixes

--- includes/perfundimet.php
+++ includes/perfundimet.php
@@ -15,7 +15,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="fas fa-gavel text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>                            
-                                <p class="width-85 mx-auto md-width-100">Sistemi i drejtësisë është i papërgatitur për të trajtuar problematikën e pedofilisë. Vetëm në 1 nga 151 vendime për krime seksuale me të mitur nën 14-vjec në 10-vitet e fundit përmendet termi “pedofili”. Në asnjë rast nuk është dhënë dënim plotësues për trajtim mjekësor të tyre, pavarësisht se 25% e tyre janë konfirmuar nga organet e hetimit si recidivistë. </p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Sistemi i drejtësisë është i papërgatitur për të trajtuar problematikën e pedofilisë. Vetëm në 1 nga 151 vendime për krime seksuale me të mitur nën 14-vjec në 10-vitet e fundit përmendet termi “pedofili”. Në asnjë rast nuk është dhënë dënim plotësues për trajtim mjekësor të tyre, pavarësisht se 25% e tyre janë konfirmuar nga organet e hetimit si recidivistë. </p>
                             </div>        
                         </div>
                     </div>
@@ -25,7 +25,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="fas fa-user-secret text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
-                                <p class="width-85 mx-auto md-width-100">Nga 98 pedofilët që kanë rifituar lirinë asnjë prej tyre nuk i është nënshtruar mjekimit të detyruar duke u kthyer në kërcënim për fëmijët. Qytetet më të rrezikuara janë Tirana, Durrësi, Elbasani e Korça ku banojnë 61 pedofilë. Nga këta, 41 kanë kryer veprën e marrëdhënieve seksuale me të mitur, 8 prej të cilëve me dhunë. Në Tiranë ka 2 polic për gjurmimin e 48 pedofilëve të lirë.</p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Nga 98 pedofilët që kanë rifituar lirinë asnjë prej tyre nuk i është nënshtruar mjekimit të detyruar duke u kthyer në kërcënim për fëmijët. Qytetet më të rrezikuara janë Tirana, Durrësi, Elbasani e Korça ku banojnë 61 pedofilë. Nga këta, 41 kanë kryer veprën e marrëdhënieve seksuale me të mitur, 8 prej të cilëve me dhunë. Në Tiranë ka 2 polic për gjurmimin e 48 pedofilëve të lirë.</p>
                             </div>
                         </div>
                     </div>
@@ -35,7 +35,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="fas fa-home text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
-                                <p class="width-85 mx-auto md-width-100">Krimet seksuale me të miturit kryhen më së shumti prej personave të njohur dhe familjarët. 60 nga 151 raste ose 40.5% e abuzimeve seksuale me të miturit vijnë prej komshinjve ose të afërmëve. Prej tyre janë kryer 21 veprat e turpshme dhe marrëdhënie seksuale me 12 raste, nga të cilat 7 me dhunë.</p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Krimet seksuale me të miturit kryhen më së shumti prej personave të njohur dhe familjarët. 60 nga 151 raste ose 40.5% e abuzimeve seksuale me të miturit vijnë prej komshinjve ose të afërmëve. Prej tyre janë kryer 21 veprat e turpshme dhe marrëdhënie seksuale me 12 raste, nga të cilat 7 me dhunë.</p>
                             </div>   
                         </div>
                     </div>
@@ -45,7 +45,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="fas fa-child text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
-                                <p class="width-85 mx-auto md-width-100">Në 16 cështje të krimeve seksuale me të miturit mes prindit dhe fëmijës vetëm  Ajo që mund të evidentohet si problematike nga arsyetimet në vendimet e gjykatave shqiptare ka të bëjë me faktin se nga 16 krime seksuale prind-fëmijë, vetëm gjykata e Dibrës në vitin 2016, krahas dënimit të babait pedofil me 15 vite burg, ka dhënë një dënim plotësues për heqjen e përgjegjësisë prindërore prej tij.</p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Në 16 cështje të krimeve seksuale me të miturit mes prindit dhe fëmijës vetëm  Ajo që mund të evidentohet si problematike nga arsyetimet në vendimet e gjykatave shqiptare ka të bëjë me faktin se nga 16 krime seksuale prind-fëmijë, vetëm gjykata e Dibrës në vitin 2016, krahas dënimit të babait pedofil me 15 vite burg, ka dhënë një dënim plotësues për heqjen e përgjegjësisë prindërore prej tij.</p>
                             </div>   
                         </div>
                     </div>
@@ -55,7 +55,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="fas fa-users text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
-                                <p class="width-85 mx-auto md-width-100">Nga 128 hetime të nisura gjatë periudhës Janar 2014 - Qershor 2019 për pedofili online (nenin 117) vetëm 14-prej tyre kanë përfunduar, nga të cilat 12 janë pushuar dhe dy janë dërguar në gjyq. Këto të fundit kanë përfunduar me një vendim dënimi. Prokurorë në Tiranë pohojnë se materialiet e referuar nga ana e policisë s’kanë prova të mjaftueshme, kreu i CRCA-së: Policia s’ka teknologjinë e duhur! </p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Nga 128 hetime të nisura gjatë periudhës Janar 2014 - Qershor 2019 për pedofili online (nenin 117) vetëm 14-prej tyre kanë përfunduar, nga të cilat 12 janë pushuar dhe dy janë dërguar në gjyq. Këto të fundit kanë përfunduar me një vendim dënimi. Prokurorë në Tiranë pohojnë se materialiet e referuar nga ana e policisë s’kanë prova të mjaftueshme, kreu i CRCA-së: Policia s’ka teknologjinë e duhur! </p>
                             </div>   
                         </div>
                     </div>
@@ -65,7 +65,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="fas fa-balance-scale text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
-                                <p class="width-85 mx-auto md-width-100">Legjislacioni Shqiptar ka mungesa evidente për dënimin e krimeve seksuale me të mitur online. Cdo formë e abuzimeve kategorizohet vetëm në nenin 117 si Pornografi. Ndërkohë që Kodi Penal nuk parashikon aspak vepra “grooming” apo “sexting” si në shumë vende të tjera.</p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Legjislacioni Shqiptar ka mungesa evidente për dënimin e krimeve seksuale me të mitur online. Cdo formë e abuzimeve kategorizohet vetëm në nenin 117 si Pornografi. Ndërkohë që Kodi Penal nuk parashikon aspak vepra “grooming” apo “sexting” si në shumë vende të tjera.</p>
                             </div>   
                         </div>
                     </div>
@@ -75,7 +75,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="fas fa-wifi text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
-                                <p class="width-85 mx-auto md-width-100">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
                             </div>   
                         </div>
                     </div>
@@ -85,7 +85,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
-                                <p class="width-85 mx-auto md-width-100">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
                             </div>   
                         </div>
                     </div>
@@ -95,7 +95,7 @@
                         <div class="feature-box">
                             <div class="content">
                                 <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
-                                <p class="width-85 mx-auto md-width-100">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
+                                <p class="width-85 mx-auto md-width-100 text-left">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
                             </div>   
                         </div>
                     </div>
