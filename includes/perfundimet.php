2c6a4ca5564662ef3e564b1e3c960c6922fac96f
Erion Elezi <erion.elezi1@yahoo.com>
2020-04-07 20:16:02 +0200

    perfundimet

--- includes/perfundimet.php
+++ includes/perfundimet.php
@@ -1,6 +1,6 @@
 <!-- start process step style 01 section -->
-<section id="konkluzione" class="wow fadeIn">
-    <div class="container">
+<section class="wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
+<div class="container">
         <div class="row justify-content-center">
             <div class="col-12 col-lg-7 text-center margin-40px-bottom sm-margin-40px-bottom">
                 <div class="position-relative overflow-hidden w-100">
@@ -8,64 +8,98 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <!-- start process step item -->
-            <div class="col-12 col-md-4 text-center feature-box-10 sm-margin-20px-bottom wow fadeInUp last-paragraph-no-margin">
-                <div class="d-inline-block padding-five-all margin-10px-bottom">
-                    <span class="number alt-font border-width-2 border-all border-color-deep-pink rounded-circle letter-spacing-minus-1 text-extra-dark-gray">01</span>
+            <div class="container">
+                <div class="row">
+                    <!-- feature box item-->
+                    <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-browser text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>                            
+                                <p class="width-85 mx-auto md-width-100">Sistemi i drejtësisë është i papërgatitur për të trajtuar problematikën e pedofilisë. Vetëm në 1 nga 151 vendime për krime seksuale me të mitur nën 14-vjec në 10-vitet e fundit përmendet termi “pedofili”. Në asnjë rast nuk është dhënë dënim plotësues për trajtim mjekësor të tyre, pavarësisht se 25% e tyre janë konfirmuar nga organet e hetimit si recidivistë. </p>
+                            </div>        
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
+                    <!-- feature box item-->
+                    <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-book-open text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
+                                <p class="width-85 mx-auto md-width-100">Nga 98 pedofilët që kanë rifituar lirinë asnjë prej tyre nuk i është nënshtruar mjekimit të detyruar duke u kthyer në kërcënim për fëmijët. Qytetet më të rrezikuara janë Tirana, Durrësi, Elbasani e Korça ku banojnë 61 pedofilë. Nga këta, 41 kanë kryer veprën e marrëdhënieve seksuale me të mitur, 8 prej të cilëve me dhunë. Në Tiranë ka 2 polic për gjurmimin e 48 pedofilëve të lirë.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
+                    <!-- feature box item-->
+                    <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
+                                <p class="width-85 mx-auto md-width-100">Krimet seksuale me të miturit kryhen më së shumti prej personave të njohur dhe familjarët. 60 nga 151 raste ose 40.5% e abuzimeve seksuale me të miturit vijnë prej komshinjve ose të afërmëve. Prej tyre janë kryer 21 veprat e turpshme dhe marrëdhënie seksuale me 12 raste, nga të cilat 7 me dhunë.</p>
+                            </div>   
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
+                      <!-- feature box item-->
+                      <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
+                                <p class="width-85 mx-auto md-width-100">Në 16 cështje të krimeve seksuale me të miturit mes prindit dhe fëmijës vetëm  Ajo që mund të evidentohet si problematike nga arsyetimet në vendimet e gjykatave shqiptare ka të bëjë me faktin se nga 16 krime seksuale prind-fëmijë, vetëm gjykata e Dibrës në vitin 2016, krahas dënimit të babait pedofil me 15 vite burg, ka dhënë një dënim plotësues për heqjen e përgjegjësisë prindërore prej tij.</p>
+                            </div>   
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
+                      <!-- feature box item-->
+                      <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
+                                <p class="width-85 mx-auto md-width-100">Nga 128 hetime të nisura gjatë periudhës Janar 2014 - Qershor 2019 për pedofili online (nenin 117) vetëm 14-prej tyre kanë përfunduar, nga të cilat 12 janë pushuar dhe dy janë dërguar në gjyq. Këto të fundit kanë përfunduar me një vendim dënimi. Prokurorë në Tiranë pohojnë se materialiet e referuar nga ana e policisë s’kanë prova të mjaftueshme, kreu i CRCA-së: Policia s’ka teknologjinë e duhur! </p>
+                            </div>   
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
+                      <!-- feature box item-->
+                      <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
+                                <p class="width-85 mx-auto md-width-100">Legjislacioni Shqiptar ka mungesa evidente për dënimin e krimeve seksuale me të mitur online. Cdo formë e abuzimeve kategorizohet vetëm në nenin 117 si Pornografi. Ndërkohë që Kodi Penal nuk parashikon aspak vepra “grooming” apo “sexting” si në shumë vende të tjera.</p>
+                            </div>   
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
+                      <!-- feature box item-->
+                      <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
+                                <p class="width-85 mx-auto md-width-100">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
+                            </div>   
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
+                      <!-- feature box item-->
+                      <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
+                                <p class="width-85 mx-auto md-width-100">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
+                            </div>   
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
+                     <!-- feature box item-->
+                     <div class="col-12 col-md-4 sm-margin-five-bottom last-paragraph-no-margin wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="feature-box">
+                            <div class="content">
+                                <i class="icon-wallet text-medium-gray icon-large margin-25px-bottom md-margin-15px-bottom"></i>
+                                <p class="width-85 mx-auto md-width-100">Fëmijët në Kosovë janë në kontakt të vazhdueshëm me përmbajtjet seksuale në internet dhe rrjetet sociale. Rreth 29% për qind e të miturve dhe fëmijëve të moshës 11-16 vjeç kanë parë të paktën dy herë në javë imazhe me përmbajtje seksuale në Kosovë në 12 muajt e fundit. </p>
+                            </div>   
+                        </div>
+                    </div>
+                    <!-- end feature box item-->
                 </div>
-                <div class="alt-font text-medium text-extra-dark-gray margin-10px-bottom">Konluzioni 1</div>
-                <p class="width-75 md-width-85 mx-auto">Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been standard dummy.</p>
             </div>
-            <!-- end process step item -->
-            <!-- start process step item -->
-            <div class="col-12 col-md-4 text-center feature-box-10 sm-margin-20px-bottom wow fadeInUp last-paragraph-no-margin" data-wow-delay="0.2s">
-                <div class="d-inline-block padding-five-all margin-10px-bottom">
-                    <span class="number number-center alt-font border-width-2 border-all border-color-deep-pink rounded-circle letter-spacing-minus-1 text-extra-dark-gray">02</span>
-                </div>
-                <div class="alt-font text-medium text-extra-dark-gray margin-10px-bottom">Konluzioni 2</div>
-                <p class="width-75 md-width-85 mx-auto">Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been standard dummy.</p>
-            </div>
-            <!-- end process step item -->
-            <!-- start process step item -->
-            <div class="col-12 col-md-4 text-center feature-box-10 wow fadeInUp last-paragraph-no-margin" data-wow-delay="0.4s">
-                <div class="d-inline-block padding-five-all margin-10px-bottom">
-                    <span class="number alt-font border-width-2 border-all border-color-deep-pink rounded-circle letter-spacing-minus-1 text-extra-dark-gray">03</span>
-                </div>
-                <div class="alt-font text-medium text-extra-dark-gray margin-10px-bottom">Konluzioni 3</div>
-                <p class="width-75 md-width-85 mx-auto">Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been standard dummy.</p>
-            </div>
-            <!-- end process step item -->
-        </div>
-        <div class="row margin-40px-top">
-            <!-- start process step item -->
-            <div class="col-12 col-md-4 text-center feature-box-10 sm-margin-20px-bottom wow fadeInUp last-paragraph-no-margin">
-                <div class="d-inline-block padding-five-all margin-10px-bottom">
-                    <span class="number alt-font border-width-2 border-all border-color-deep-pink rounded-circle letter-spacing-minus-1 text-extra-dark-gray">04</span>
-                </div>
-                <div class="alt-font text-medium text-extra-dark-gray margin-10px-bottom">Konluzioni 4</div>
-                <p class="width-75 md-width-85 mx-auto">Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been standard dummy.</p>
-            </div>
-            <!-- end process step item -->
-            <!-- start process step item -->
-            <div class="col-12 col-md-4 text-center feature-box-10 sm-margin-20px-bottom wow fadeInUp last-paragraph-no-margin " data-wow-delay="0.2s">
-                <div class="d-inline-block padding-five-all margin-10px-bottom">
-                    <span class="number number-center alt-font border-width-2 border-all border-color-deep-pink rounded-circle letter-spacing-minus-1 text-extra-dark-gray">05</span>
-                </div>
-                <div class="alt-font text-medium text-extra-dark-gray margin-10px-bottom">Konluzioni 5</div>
-                <p class="width-75 md-width-85 mx-auto">Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been standard dummy.</p>
-            </div>
-            <!-- end process step item -->
-            <!-- start process step item -->
-            <div class="col-12 col-md-4 text-center feature-box-10 wow fadeInUp last-paragraph-no-margin" data-wow-delay="0.4s">
-                <div class="d-inline-block padding-five-all margin-10px-bottom">
-                    <span class="number alt-font border-width-2 border-all border-color-deep-pink rounded-circle letter-spacing-minus-1 text-extra-dark-gray">06</span>
-                </div>
-                <div class="alt-font text-medium text-extra-dark-gray margin-10px-bottom">Konluzioni 6</div>
-                <p class="width-75 md-width-85 mx-auto">Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been standard dummy.</p>
-            </div>
-            <!-- end process step item -->
-        </div>
-    </div>
-</section>
-<!-- start process step style 01 section -->
+        </section>
